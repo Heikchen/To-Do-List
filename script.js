bbf8359c37621a5f3d87dfeb0580766fb1deebb7
Heike Kasper <heike.kasper@kadelo.de>
2022-06-30 09:22:28 +0200

    added and styles main section

---
+++ script.js
@@ -0,0 +1,6 @@
+//Different Boxes for Different Lists for example Grocceries, work etc.
+//checkboxes change if task is done and different class
+//Priority of tasks /drag and drop / highlight
+//clean list
+//add new tasks
+//search for tasks
