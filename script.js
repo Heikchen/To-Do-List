b3b1f625af7c130d166df78d96835e233b8c1b3c
Heike Kasper <heike.kasper@kadelo.de>
2022-07-10 13:35:35 +0200

    function add new to do list

--- script.js
+++ script.js
@@ -2,24 +2,73 @@
 const newListBtn = document.getElementById("new-list");
 const newListDiv = document.querySelector(".list");
 const newListArray = [newListDiv];
-
+let todoLists = [];
+const newlist = document.querySelector(".list-container");
+let listNumber = 0;
 function CreateNewList() {
-  newListDiv.style.display = "flex";
-  let listElement = newListDiv.cloneNode(true);
-  listElement.id = "new-List" + newListArray.length;
-  document.querySelector(".list-container").appendChild(listElement);
-  const newListChildren = document.querySelectorAll(
-    "#new-List" + newListArray.length + " > *"
+  listNumber++;
+  const todoList = {
+    active: true,
+    id: Date.now(),
+    value: "",
+    number: listNumber,
+  };
+  todoLists.push(todoList);
+
+  newlist.insertAdjacentHTML(
+    "beforeend",
+    `<div class="list">
+<input id="${todoList.id}" class ="list-input"value="">
+<p id="number-of-tasks"> 5</p>
+        </div>`
   );
-  const newListInput = newListChildren[0];
-  console.log(newListChildren);
-  newListInput.removeAttribute("id");
-  newListInput.id = "list-each" + newListArray.length;
-  document.getElementById("list-each" + newListArray.length).focus();
-  newListArray.push(listElement);
-  newListDiv.style.display = "none";
+  let inputList = document.getElementById(todoList.id);
+  console.log(todoLists);
+  inputList.focus();
+  inputList.addEventListener("change", function (event) {
+    const inputId = event.target.id;
+    updatePropertyValue(inputId);
+  });
 }
-newListBtn.onclick = CreateNewList;
+
+newListBtn.addEventListener("click", function () {
+  for (let i = 0; i < todoLists.length; i++) {
+    todoLists[i].active = false;
+  }
+  CreateNewList();
+});
+
+newlist.addEventListener("click", function (event) {
+  if (event.target.classList.contains("list-input")) {
+    const inputId = event.target.id;
+    changeList(inputId);
+    let listInput = document.getElementById(inputId);
+
+    listInput.addEventListener("change", function (event) {
+      const inputId = event.target.id;
+      updatePropertyValue(inputId);
+    });
+  }
+});
+function changeList(elementId) {
+  for (let i = 0; i < todoLists.length; i++) {
+    todoLists[i].active = false;
+  }
+  const index = todoLists.findIndex((item) => item.id === Number(elementId));
+  todoLists[index].active = true;
+  updatePropertyValue(elementId);
+}
+
+const todoHeader = document.querySelector(".task-list");
+function updatePropertyValue(elementId) {
+  const index = todoLists.findIndex((item) => item.id === Number(elementId));
+  const newValue = document.getElementById(elementId);
+  if (newValue.value !== "") {
+    todoLists[index].value = newValue.value;
+    todoHeader.textContent = todoLists[index].value;
+  }
+}
+
 //checkboxes change if task is done and different class
 //Priority of tasks /drag and drop / highlight
 //clean list
